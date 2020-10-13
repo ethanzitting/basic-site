1987799950d552d073c965e8b78dc74cc26034d7
Ethan Zitting <ethanzitting@gmail.com>
2020-10-13 09:14:13 -0600

    Fixed functionality. It works!

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
+// Modules
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+// Initiates Server
 http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  console.log(filename);
-  if (filename = './') {
-	  filename = './index.html';
-  }
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+	// Parses requested url
+	var q = url.parse(req.url, true);
+	console.log(q.pathname);
+	filename = "." + q.pathname;
+	console.log(filename);
+  
+	// Load the appropriate webpage
+	if (filename == "./") {
+		// Load Home
+		fs.readFile("index.html", function(err, data) {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			console.log(typeof(data));
+			res.write(data);
+			return res.end();
+		});
+	} else if (filename == "./about.html") {
+		// Load About
+		fs.readFile(filename, function(err, data) {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(data);
+			return res.end();
+		});
+	} else if (filename == "./contact-me.html") {
+		// Load Contact-Me
+		fs.readFile(filename, function(err, data) {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(data);
+			return res.end();
+		});
+	} else {
+		// Load 404
+		fs.readFile("./404.html", function(err, data) {
+			res.writeHead(404, {'Content-Type': 'text/html'});
+			res.write(data);
+			return res.end();
+		});
+	}
 }).listen(8080);
